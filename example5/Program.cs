ef2675301dae622d2258b37a1346c0472bd6e915
nika <nickykh3002@gmail.com>
2023-03-02 19:24:26 +0500

    if else

---
+++ example5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("здравствуйте");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
