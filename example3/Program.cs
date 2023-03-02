54e022265cf2650399e4cfb29e1d0b7c93ecf8c2
nika <nickykh3002@gmail.com>
2023-03-02 18:57:54 +0500

    random

--- example3/Program.cs
+++ example3/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,0);  // 1 2 3 ... 9
+Console.Write(numberA);
+int numberB = new Random().Next(1,10);
+Console.Write(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
